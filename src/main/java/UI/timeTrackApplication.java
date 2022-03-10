40625334b783b73384bcbce764387e25efa1c326
Vancuver <lovedjekey@gmail.com>
2022-03-10 19:04:55 +0200

    setup

---
+++ src/main/java/UI/timeTrackApplication.java
@@ -0,0 +1,13 @@
+package UI;
+
+public class timeTrackApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Time Track!");
+        System.out.println("Please make a choice:");
+
+        System.out.println("1. Add");
+        System.out.println("2. Get All");
+        System.out.println("3. Delete");
+    }
+}
